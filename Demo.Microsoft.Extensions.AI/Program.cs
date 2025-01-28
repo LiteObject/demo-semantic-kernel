83ec8838ea553cba21903772ca47fda3638dafa8
LiteObject <Lite.Object@gmail.com>
2025-01-27 21:36:00 -0600

    Refactor IChatClient instantiation and add embeddings
    Updated `Program.cs` to use a `using` statement for `IChatClient` to improve resource management. Commented out the `CreateChatClient` method and introduced a new `GeneratedEmbeddings` method that generates and prints embeddings for a given input.

--- Demo.Microsoft.Extensions.AI/Program.cs
+++ Demo.Microsoft.Extensions.AI/Program.cs
@@ -8,21 +8,34 @@ namespace Demo.Microsoft.Extensions.AI
     {
         static async Task Main(string[] args)
         {
-            IChatClient client =
+            using IChatClient client =
                 new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.2:latest");
 
             var response = await client.CompleteAsync("What is AI? Give me a short answer.");
             Console.WriteLine(response.Message);
+
+            // await GeneratedEmbeddings();
         }
 
         // install package Microsoft.Extensions.AI.Abstractions
 
-        private static IChatClient CreateChatClient(Arguments arguments)
+        //private static IChatClient CreateChatClient(Arguments arguments)
+        //{
+        //    if (arguments.Provider.Equals("ollama", StringComparison.OrdinalIgnoreCase))
+        //        return new OllamaApiClient(arguments.Uri, arguments.Model);
+        //    else
+        //        return new OpenAIChatClient(new OpenAI.OpenAIClient(arguments.ApiKey), arguments.Model);
+        //}
+
+        private static async Task GeneratedEmbeddings()
         {
-            if (arguments.Provider.Equals("ollama", StringComparison.OrdinalIgnoreCase))
-                return new OllamaApiClient(arguments.Uri, arguments.Model);
-            else
-                return new OpenAIChatClient(new OpenAI.OpenAIClient(arguments.ApiKey), arguments.Model);
+            IEmbeddingGenerator<string, Embedding<float>> generator =
+                new OllamaEmbeddingGenerator(new Uri("http://localhost:11434/"), "nomic-embed-text:latest");
+
+            var embedding = await generator.GenerateAsync(["What is AI?"]);
+
+            Console.WriteLine(string.Join(", ", embedding[0].Vector.ToArray()));
         }
     }
 }
+    
